0c6d98114832583954d6167b739f2809ea86688b
Douglas Bacon <dugbdev@gmail.com>
2018-04-15 07:47:06 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ hello: 'world' });
+});
+app.get('/sup', (req, res) => {
+	res.send({ wassup: 'world' });
 });
 
 const PORT = process.env.PORT || 5000;
